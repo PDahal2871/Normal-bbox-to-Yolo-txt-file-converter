875590a0898378be94ab198dd54054ee01c8635d
Prakash Dahal <59336638+PDahal2871@users.noreply.github.com>
2021-11-19 16:09:48 +0545

    Update yolo_to_normal.py

--- yolo_to_normal.py
+++ yolo_to_normal.py
@@ -1,21 +1,20 @@
 import cv2, os, shutil
 
-def convert_to_normal(dt,dh,dw):
-#     fl = open(label_path, 'r')
-#     data = fl.readlines()
-#     fl.close()
+"""
+-->  This function takes each line of txt file 
+-- > returns actual bbox and class from that line. (Converted from Yolo format to OpenCV format)
+--> cls is the integer form of yolo class, bbox is (x1,y1,x2,y2) where (x1,y1) represents top left position of image and (x2,y2) represents bottom right position.
+"""
 
-#     for dt in data:
+def convert_to_normal(dt,dh,dw):
 
     # Split string to float
-    classes = str(dt.split(' ')[0])
+    cls = int(dt.split(' ')[0])
     x = float(dt.split(' ')[1])
     y = float(dt.split(' ')[2])
     w = float(dt.split(' ')[3])
     h = float(dt.split(' ')[4])
 
-    # Taken from https://github.com/pjreddie/darknet/blob/810d7f797bdb2f021dbe65d2524c2ff6b8ab5c8b/src/image.c#L283-L291
-    # via https://stackoverflow.com/questions/44544471/how-to-get-the-coordinates-of-the-bounding-box-in-yolo-object-detection#comment102178409_44592380
     l = int((x - w / 2) * dw)
     r = int((x + w / 2) * dw)
     t = int((y - h / 2) * dh)
@@ -31,7 +30,7 @@ def convert_to_normal(dt,dh,dw):
         b = dh - 1
 
 
-    return classes, (l,t,r,b)
+    return cls, (l,t,r,b)
 
 
 im = cv2.imread(os.path.join('test.png'))
@@ -40,8 +39,7 @@ dh, dw, _ = im.shape
 with open(os.path.join('test.txt'),'r') as f:
     for lines in f.readlines():
         cls, bbox = convert_to_normal(lines, dh,dw)
-#         if cls in ['35','36']:
-#             continue
-#         nm = class_dict[cls]
-        img_crop = im[bbox[1]:bbox[3], bbox[0]:bbox[2]]
-        cv2.imwrite(os.path.join('test.png'), img_crop)
+        print('class index -->', cls)
+        print('Bounding box -->',bbox)
+#         img_crop = im[bbox[1]:bbox[3], bbox[0]:bbox[2]]
+
